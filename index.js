7ec3e5ea8899c41bda68b25848ae07634f51ad82
nayeem-miah <nayeem5113a@gmail.com>
2025-02-26 15:50:04 +0600

    payment data post in db

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.json());
 
 // console.log(process.env.DB_PASS);
 
-// const uri = 'mongodb://localhost:27017'
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bomlehy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = 'mongodb://localhost:27017/'
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bomlehy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -37,7 +37,7 @@ async function run() {
     const articleCollection = client.db("OnTimeNewsDB").collection("articles");
     const userCollection = client.db("OnTimeNewsDB").collection("users");
     const publisherCollection = client.db("OnTimeNewsDB").collection("publisher");
-    // const paymentCollection = client.db("OnTimeNewsDB").collection("payment");
+    const paymentCollection = client.db("OnTimeNewsDB").collection("payment");
 
     // middlewares verify token
     const verifyToken = (req, res, next) => {
@@ -299,16 +299,17 @@ async function run() {
     });
 
     // -----------------payment info --------------
-    // app.post("/payment", async (req, res) => {
-    //   const newPayment = req.body;
-    //   const result = await paymentCollection.insertOne(newPayment);
-    //   res.send(result);
-    // });
+    app.post("/payment", async (req, res) => {
+      const newPayment = req.body;
+      const result = await paymentCollection.insertOne(newPayment);
+      console.log(result);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
